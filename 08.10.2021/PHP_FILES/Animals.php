675261af6b8835724799935c1fa750fbe4493a9b
Julius Sever <iulius_severus@mail.ru>
2021-10-11 00:13:09 +0500

    08.10.2021

---
+++ 08.10.2021/PHP_FILES/Animals.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="../Styles/style_animals.css">
+</head>
+<body>
+    <table class="my_table">
+        <caption align="bottom">Таблица животных</caption>
+        <tr>
+           <td>Птица</td>
+            <td ><img src="../Images/bird.png"> </td>
+        </tr>
+        <tr>
+         <td colspan="2">Хомячок<img align="middle" src="../Images/hamster.png"></td>
+        </tr>
+        <tr>
+            <td>Божья коровка</td>
+            <td><img src="..//Images/ladybug.png"></td>
+        </tr>
+    </table>
+</body>
+</html>
+
---
+++ 08.10.2021/PHP_FILES/Table_of_Mendeleev.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Periodic_table_of_Mendeleev</title>
+    <link rel="stylesheet" href="../Styles/styles_of_table.css">
+</head>
+<body class="base">
+<div class="Sc">
+    <table frame="border" width="80" align="right" bgcolor="aqua">
+        <tr>
+            <td align="left" valign="top" rowspan="2">21 44.956</td>
+            <td align="right" valign="top">
+                <a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%B9" target="_blank">
+                    <span class="name">Sc</span>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td align="right">Скандий</td>
+        </tr>
+    </table>
+</div>
+<div class="Ti">
+    <table frame="border" width="115" align="left" bgcolor="aqua" >
+        <tr>
+            <td align="left" valign="top" rowspan="2">22  <br>47.90</br></td>
+            <td align="right" valign="top">
+                <a href="https://ru.wikipedia.org/wiki/%D0%A2%D0%B8%D1%82%D0%B0%D0%BD_(%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82)" target="_blank">
+                    <span class="name">Ti</span>
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td align="right">Титан</td>
+        </tr>
+    </table>
+</div>
+<div class="Ga">
+    <table frame="border" width="115" align="right" bgcolor="yellow">
+        <tr>
+            <td align="left" valign="top" >
+                <a href="https://ru.wikipedia.org/wiki/%D0%93%D0%B0%D0%BB%D0%BB%D0%B8%D0%B9" target="_blank">
+                    <span class="name">Ga</span>
+                </a>
+            </td>
+            <td align="right" valign="top" rowspan="2">31 69.72</td>
+        </tr>
+        <tr>
+            <td align="right">Галлий</td>
+        </tr>
+    </table>
+</div>
+<div class="Ge">
+    <table frame="border" width="115" align="left" bgcolor="yellow">
+        <tr>
+            <td align="left" valign="top">
+                <a href="https://ru.wikipedia.org/wiki/%D0%93%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B8%D0%B9" target="_blank">
+                    <span class="name">Ge</span>
+                </a>
+            </td>
+            <td align="right" valign="top" rowspan="2">32 72.59</td>
+        </tr>
+        <tr>
+            <td align="right">Германий</td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
